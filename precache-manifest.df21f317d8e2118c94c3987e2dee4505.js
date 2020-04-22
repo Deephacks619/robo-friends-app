70b76cde30805725effd3b046219c8ce563cf863
deephacks619 <deepakkyatham@gmai.com>
2020-04-22 19:45:32 +0530

    Updates

---
+++ precache-manifest.df21f317d8e2118c94c3987e2dee4505.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60284a13c0796df4d201c90348e878d",
+    "url": "/Deephacks619.github.io/robo-friends-app/index.html"
+  },
+  {
+    "revision": "483a63b82d14199ef49c",
+    "url": "/Deephacks619.github.io/robo-friends-app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "228b70f3ffd36ef7440b",
+    "url": "/Deephacks619.github.io/robo-friends-app/static/css/main.0af4e45f.chunk.css"
+  },
+  {
+    "revision": "483a63b82d14199ef49c",
+    "url": "/Deephacks619.github.io/robo-friends-app/static/js/2.b22ba672.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Deephacks619.github.io/robo-friends-app/static/js/2.b22ba672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "228b70f3ffd36ef7440b",
+    "url": "/Deephacks619.github.io/robo-friends-app/static/js/main.42696461.chunk.js"
+  },
+  {
+    "revision": "852211ad689ed765afef",
+    "url": "/Deephacks619.github.io/robo-friends-app/static/js/runtime-main.a8e7e9c5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Deephacks619.github.io/robo-friends-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
